cbc8cbd5995b3fa6d1e57d4b9007eeb303552c56
Jerry Z <jdz.uke2@gmail.com>
2024-01-21 15:18:04 +0000

    add Note model and register it with the admin site

--- notes_project/notes/admin.py
+++ notes_project/notes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from notes.models import Note
 
-# Register your models here.
+
+class NoteAdmin(admin.ModelAdmin):
+    fields = ("user", "title", "content", "created", "modified")
+    list_display = ("id", "user", "title", "created", "modified")
+    list_filter = ("user__username",)
+    readonly_fields = ("created", "modified")
+    search_fields = ("id", "user__username", "title")
+
+
+admin.site.register(Note, NoteAdmin)
--- notes_project/notes/models.py
+++ notes_project/notes/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="notes",
+        verbose_name="User",
+    )
+    title = models.CharField("Title", max_length=255)
+    content = models.TextField("Content", blank=True)
+    created = models.DateTimeField("Created", blank=True, auto_now_add=True)
+    modified = models.DateTimeField(
+        "Modified", blank=True, null=True, auto_now=True
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Note"
