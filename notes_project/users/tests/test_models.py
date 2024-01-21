f31b5b438584d66c5328845522b583c1ce5bad8d
Jerry Z <jdz.uke2@gmail.com>
2024-01-21 17:39:45 +0000

    set up model tests

---
+++ notes_project/users/tests/test_models.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+class NoteTest(TestCase):
+    def test_create_note(self):
+        pass
+
+    def test_string_representation_is_the_same_as_the_title(self):
+        pass
+
+    def test_content_preview_cuts_off_at_180_characters(self):
+        pass
