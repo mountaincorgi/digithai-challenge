0acee0008945f5715f86e6eaa86f1e94bcf9500a
Jerry Z <jdz.uke2@gmail.com>
2024-01-21 17:44:05 +0000

    Move notes models tests to the correct directory and outline some tests for forms

---
+++ notes_project/notes/tests/test_forms.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+
+class NoteFormTest(TestCase):
+    def test_form_submit_success(self):
+        pass
+
+    def test_title_field_is_required(self):
+        pass
+
+    def test_title_field_strips_whitespace(self):
+        pass
+
+    def test_content_field_is_optional(self):
+        pass
+
+    def test_content_field_strips_whitespace(self):
+        pass
