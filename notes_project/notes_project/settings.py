d278d6e942428093d1a456921aa9277251480bf2
Jerry Z <jdz.uke2@gmail.com>
2024-01-21 15:07:06 +0000

    Create notes app

--- notes_project/notes_project/settings.py
+++ notes_project/notes_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "notes.apps.NotesConfig",
     "users.apps.UsersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
