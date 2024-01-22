c4789de267013efcfda148463208efb7968383c1
Jerry Z <jdz.uke2@gmail.com>
2024-01-22 16:38:02 +0000

    Add preview content method to the Note model and add some basic test for the model

--- notes_project/notes/models.py
+++ notes_project/notes/models.py
@@ -10,7 +10,7 @@ class Note(models.Model):
         related_name="notes",
         verbose_name="User",
     )
-    title = models.CharField("Title", max_length=255)
+    title = models.CharField("Title", max_length=140)
     content = models.TextField("Content", blank=True)
     created = models.DateTimeField("Created", blank=True, auto_now_add=True)
     modified = models.DateTimeField(
@@ -22,6 +22,14 @@ class Note(models.Model):
 
     def get_absolute_url(self):
         return reverse("note-detail", kwargs={"pk": self.pk})
+
+    @property
+    def preview_content(self):
+        """Shorten the content shown on certain pages."""
+
+        if len(self.content) > 180:
+            return self.content[:177] + "..."
+        return self.content
 
     class Meta:
         verbose_name = "Note"
--- notes_project/notes/tests/test_models.py
+++ notes_project/notes/tests/test_models.py
@@ -1,12 +1,47 @@
 from django.test import TestCase
+from notes.factories import NoteFactory
 
 
 class NoteTest(TestCase):
-    def test_create_note(self):
-        pass
+    @classmethod
+    def setUpTestData(cls):
+        cls.note = NoteFactory()
 
     def test_string_representation_is_the_same_as_the_title(self):
-        pass
+        self.assertEqual(str(self.note), self.note.title)
+
+    def test_first_name_label(self):
+        field_label = self.note._meta.get_field("title").verbose_name
+        self.assertEqual(field_label, "Title")
+
+    def test_content_label(self):
+        field_label = self.note._meta.get_field("content").verbose_name
+        self.assertEqual(field_label, "Content")
+
+    def test_created_label(self):
+        field_label = self.note._meta.get_field("created").verbose_name
+        self.assertEqual(field_label, "Created")
+
+    def test_modified_label(self):
+        field_label = self.note._meta.get_field("modified").verbose_name
+        self.assertEqual(field_label, "Modified")
+
+    def test_title_max_length(self):
+        max_length = self.note._meta.get_field("title").max_length
+        self.assertEqual(max_length, 140)
 
     def test_content_preview_cuts_off_at_180_characters(self):
-        pass
+        note = NoteFactory(
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus dictum massa, sit amet vehicula purus laoreet nec. Nam iaculis dignissim eros, et elementum ex imperdiet id."
+        )
+        self.assertEqual(
+            note.preview_content,
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus dictum massa, sit amet vehicula purus laoreet nec. Nam iaculis dignissim eros, et elementum ex imperd...",
+        )
+
+    def test_content_preview_under_180_characters_stays_the_same(self):
+        note = NoteFactory(content="Less than 180 characters!")
+        self.assertEqual(note.content, "Less than 180 characters!")
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.note.get_absolute_url(), "/notes/1/")
