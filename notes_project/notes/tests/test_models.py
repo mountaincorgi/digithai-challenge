a7a4481dd07d4e7eb8f7c3f5e3009e7fb6610495
Jerry Z <jdz.uke2@gmail.com>
2024-01-23 03:09:23 +0000

    Clean up note model tests, use 'assert' from pytest

--- notes_project/notes/tests/test_models.py
+++ notes_project/notes/tests/test_models.py
@@ -8,40 +8,40 @@ class NoteTest(TestCase):
         cls.note = NoteFactory()
 
     def test_string_representation_is_the_same_as_the_title(self):
-        self.assertEqual(str(self.note), self.note.title)
+        assert str(self.note) == self.note.title
 
     def test_first_name_label(self):
         field_label = self.note._meta.get_field("title").verbose_name
-        self.assertEqual(field_label, "Title")
+        assert field_label == "Title"
 
     def test_content_label(self):
         field_label = self.note._meta.get_field("content").verbose_name
-        self.assertEqual(field_label, "Content")
+        assert field_label == "Content"
 
     def test_created_label(self):
         field_label = self.note._meta.get_field("created").verbose_name
-        self.assertEqual(field_label, "Created")
+        assert field_label == "Created"
 
     def test_modified_label(self):
         field_label = self.note._meta.get_field("modified").verbose_name
-        self.assertEqual(field_label, "Modified")
+        assert field_label == "Modified"
 
     def test_title_max_length(self):
         max_length = self.note._meta.get_field("title").max_length
-        self.assertEqual(max_length, 140)
+        assert max_length == 140
 
     def test_content_preview_cuts_off_at_180_characters(self):
         note = NoteFactory(
             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus dictum massa, sit amet vehicula purus laoreet nec. Nam iaculis dignissim eros, et elementum ex imperdiet id."
         )
-        self.assertEqual(
-            note.preview_content,
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus dictum massa, sit amet vehicula purus laoreet nec. Nam iaculis dignissim eros, et elementum ex imperd...",
+        assert (
+            note.preview_content
+            == "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus dictum massa, sit amet vehicula purus laoreet nec. Nam iaculis dignissim eros, et elementum ex imperd..."
         )
 
     def test_content_preview_under_180_characters_stays_the_same(self):
         note = NoteFactory(content="Less than 180 characters!")
-        self.assertEqual(note.content, "Less than 180 characters!")
+        assert note.content == "Less than 180 characters!"
 
     def test_get_absolute_url(self):
-        self.assertEqual(self.note.get_absolute_url(), f"/{self.note.pk}/")
+        assert self.note.get_absolute_url() == f"/{self.note.pk}/"
